6f50aedd4028edbc993ecdc163c8f7c9073a8d51
Johnson  Ekedum <johnekedum@gmail.com>
2018-07-15 16:27:51 -0700

    Add files via upload

--- init.r
+++ init.r
@@ -1,4 +1,12 @@
 
 
+ 
 install.packages("shiny", dependencies=TRUE)  
+install.packages("plotly", dependencies=TRUE)  
+install.packages("ggplot2", dependencies=TRUE)  
+install.packages("data.table", dependencies=TRUE)
+install.packages("magrittr", dependencies=TRUE)  
+install.packages("highcharter", dependencies=TRUE)  
+ 
+
 
