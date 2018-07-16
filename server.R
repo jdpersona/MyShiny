bb7aea9a081a931b0f19e140e2f3c2b090522473
Johnson  Ekedum <johnekedum@gmail.com>
2018-07-15 17:35:08 -0700

    Add files via upload

--- server.R
+++ server.R
@@ -11,65 +11,14 @@ library(shiny)
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
-  
-  output$plot <- renderHighchart({
-    
-    df_WCA = sub_data[sub_data$url == input$choose,] 
-    
+   
+  output$distPlot <- renderHighchart({
+
     highchart() %>% 
-      hc_title(text = "Total Form Submitted") %>% 
-      hc_add_series_times_values(df_WCA$Begin_Date,df_WCA$Total_form_submitted)
+      hc_title(text = "Scatter chart with size and color") %>% 
+      hc_add_series_scatter(mtcars$wt, mtcars$mpg,
+                            mtcars$drat, mtcars$hp)
     
   })
   
-  
-  output$plot2 <- renderHighchart({
-    
-    df_WCA = sub_data[sub_data$url == input$choose, ] 
-    
-    highchart() %>% 
-      hc_title(text = "Total Form Shown") %>% 
-      hc_add_series_times_values(df_WCA$Begin_Date,df_WCA$Total_form_shown)
-  })
-  
-  
-  output$plot3 <- renderHighchart({
-    
-    df_WCA = sub_data[sub_data$url == input$choose, ] 
-    
-    highchart() %>% 
-      hc_title(text = "Conversion Rate") %>% 
-      hc_add_series_times_values(df_WCA$Begin_Date,df_WCA$Conversion_rate)
-    
-  })
-  
-  
-  mytable <-  reactive({
-    df_WCA = sub_data[sub_data$url == input$choose, ]
-    
-    total_growth = round(df_WCA[df_WCA$Begin_Date == max(df_WCA$Begin_Date),]['Conversion_rate']-df_WCA[df_WCA$Begin_Date == min(df_WCA$Begin_Date),]['Conversion_rate'],0)
-    avg_conversion = round(sum(df_WCA$Conversion_rate)/length(df_WCA$Conversion_rate),0)
-    avg_form_sub = sum(df_WCA$Total_form_submitted)/length(df_WCA$Total_form_submitted)
-    avg_form_shown  = sum(df_WCA$Total_form_shown)/length(df_WCA$Total_form_shown)
-    test = data.frame( "Rate" = c(avg_form_shown, avg_form_sub, paste(avg_conversion,"percent"), paste(total_growth$Conversion_rate,"percent")), row.names = c('Average Form Shown', 'Average Form Submitted', 'Average Conversion rates', 'Total Growth') )
-    test
-  })  
-  
-  
-  
-  output$table <- DT :: renderDataTable({           
-    
-    DT::datatable(mytable(), rownames = TRUE, options = list(pageLength =4))
-  }, server=TRUE)
-  
-  
-  output$table2 <- DT :: renderDataTable({           
-    
-    DT::datatable(sub_data[sub_data$url == input$choose, ], rownames = TRUE, options = list(pageLength =10))
-    
-  }, server=TRUE)
-  
-  
-  
-}
-)
+})
--- ui.R
+++ ui.R
@@ -6,76 +6,30 @@
 # 
 #    http://shiny.rstudio.com/
 #
-library(shinythemes)
+
 library(shiny)
-library(plotly)
-library(ggplot2)
-library(data.table)
 library(magrittr)
 library(highcharter)
-
-data = read.csv('https://raw.githubusercontent.com/jdpersona/Dash_experiments/master/all_data.csv')
-# there are 10 columns
-length(colnames(data))
-
-# convert character date to real dates
-data$Begin_Date = as.Date(data$Begin_Date, "%m/%d/%y")
-data$End_Date = as.Date(data$End_Date, "%m/%d/%y")
-
-sub_data = data %>% 
-  select(Begin_Date, url, form_shown, form_submitted) %>% 
-  group_by(Begin_Date, url) %>% summarise (Total_form_shown = sum(form_shown), Total_form_submitted = sum(form_submitted)) %>% 
-  arrange(desc(url))
-
-sub_data$Conversion_rate =  round((sub_data$Total_form_submitted/sub_data$Total_form_shown) * 100, digits=0)
-
-sub_data$url = trimws(sub_data$url, which='both')
-
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
   
   # Application title
-  titlePanel("SaaS Response",windowTitle = "SaaS Exploration"),
+  titlePanel("Old Faithful Geyser Data"),
   
   # Sidebar with a slider input for number of bins 
   sidebarLayout(
     sidebarPanel(
-      width = 3,
-      selectInput(inputId ="choose", 
-                  choices = c("Watson Campaign Automation","tealeaf","digitalexperience","digitalanalytics"),    
-                  selected = "Watson Campaign Automation",label = "Choose a SaaS Product"),
-      br(),
-      br(),
-      br()
-      
-      
-      
-      
+       sliderInput("bins",
+                   "Number of bins:",
+                   min = 1,
+                   max = 50,
+                   value = 30)
     ),
-    
-    
     
     # Show a plot of the generated distribution
     mainPanel(
-      
-      tabsetPanel(
-        tabPanel("Graphs", highchartOutput("plot",height = 400, width = 1000),
-                 br(),
-                 highchartOutput("plot2",height = 400, width = 1000),
-                 br(),
-                 highchartOutput("plot3",height = 400, width = 1000),
-                 br(),
-                 DT :: dataTableOutput('table')
-                 
-        )  
-                 
-        ),
-        
-        tabPanel("Agregated Data", DT :: dataTableOutput('table2') )
-        
-      )
+      highchartOutput("distPlot")
     )
-    
   )
-)
+))
