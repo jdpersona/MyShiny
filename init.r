0190f444c626dd8e8d978f9de9e7ed1431502f70
Johnson  Ekedum <johnekedum@gmail.com>
2018-07-15 17:36:19 -0700

    Update init.r

--- init.r
+++ init.r
@@ -1,13 +1,8 @@
 
-
- 
 install.packages("shiny", dependencies=TRUE)  
-install.packages("plotly", dependencies=TRUE)  
-install.packages("ggplot2", dependencies=TRUE)  
-install.packages("data.table", dependencies=TRUE)
-install.packages("magrittr", dependencies=TRUE)  
-install.packages("highcharter", dependencies=TRUE)
-install.packages("git2r",dependencies=TRUE)
+install.packages("magrittr",dependencies=TRUE)  
+install.packages("highcharter",dependencies=TRUE)
+
  
 
 
